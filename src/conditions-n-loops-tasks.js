8a67770778fe571d7438227dffcd1694efbe4c70
letanatol <letanatol@gmail.com>
2023-12-29 02:59:55 +0100

    feat: add solution task 03_canQueenCaptureKing

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -76,8 +76,13 @@ function getMaxNumber(a, b, c) {
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  * {x: 1, y: 1}, {x: 2, y: 8} => false
  */
-function canQueenCaptureKing(/* queen, king */) {
-  throw new Error('Not implemented');
+function canQueenCaptureKing(queen, king) {
+  if (queen.x === king.x) return true;
+  if (queen.y === king.y) return true;
+  const x = Math.abs(queen.x - king.x);
+  const y = Math.abs(queen.y - king.y);
+  if (x === y) return true;
+  return false;
 }
 
 /**
