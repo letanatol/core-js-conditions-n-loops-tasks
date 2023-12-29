94fd1651ddb565ea9ae2e267b161ca7a430b5740
letanatol <letanatol@gmail.com>
2023-12-29 04:23:30 +0100

    feat: add solution task 07_isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -364,8 +364,13 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let strRevers = '';
+  for (let i = 0; i < str.length; i += 1) {
+    strRevers = str[i] + strRevers;
+  }
+  if (str === strRevers) return true;
+  return false;
 }
 
 /**
