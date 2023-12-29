def18475436f3d5fa0e8501798ecb41dfeba4cf1
letanatol <letanatol@gmail.com>
2023-12-29 03:02:09 +0100

    feat: add solution task 04_isIsoscelesTriangle

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -103,8 +103,11 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a + b > c && b + c > a && c + a > b) {
+    if (a === b || b === c || a === c) return true;
+  }
+  return false;
 }
 
 /**
